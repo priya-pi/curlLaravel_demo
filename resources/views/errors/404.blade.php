dbcf198bf7d809ecfa9da9d43f6399aaccb6d9f5
Priya T <pthummar@patoliyainfotech.com>
2023-05-27 13:03:11 +0530

    add new 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,93 +1,241 @@
 <style>
 
-* {
-font-family: Google sans, Arial;
-}
+  @import url(https://fonts.googleapis.com/css?family=opensans:500);
+  body{
+      background: #397be6;
+      color:#fff;
+      font-family: 'Open Sans', sans-serif;
+      max-height:700px;
+      overflow: hidden;
+  }
+  .c{
+      text-align: center;
+      display: block;
+      position: relative;
+      width:80%;
+      margin:100px auto;
+  }
+  ._404{
+      font-size: 220px;
+      position: relative;
+      display: inline-block;
+      z-index: 2;
+      height: 250px;
+      letter-spacing: 15px;
+  }
+  ._1{
+      text-align:center;
+      display:block;
+      position:relative;
+      letter-spacing: 12px;
+      font-size: 4em;
+      line-height: 80%;
+  }
+  ._2{
+      text-align:center;
+      display:block;
+      position: relative;
+      font-size: 20px;
+  }
+  .text{
+      font-size: 70px;
+      text-align: center;
+      position: relative;
+      display: inline-block;
+      margin: 19px 0px 0px 0px;
+      /* top: 256.301px; */
+      z-index: 3;
+      width: 100%;
+      line-height: 1.2em;
+      display: inline-block;
+  }
 
-html, body {
-margin: 0;
-padding: 0;
-}
 
-.flex-container {
-display: flex;
-justify-content: center;
-align-items: center;
-height: 100vh;
-color: white;
-animation: colorSlide 15s cubic-bezier(0.075, 0.82, 0.165, 1) infinite;
+  .btn{
+      background-color: rgb( 255, 255, 255 );
+      position: relative;
+      display: inline-block;
+      width: 358px;
+      padding: 5px;
+      z-index: 5;
+      font-size: 25px;
+      margin:0 auto;
+      color:#397be6;
+      text-decoration: none;
+      margin-right: 10px
+  }
+  .right{
+      float:right;
+      width:60%;
+  }
 
-.text-center {
-text-align: center;
-h1,
-h3 {
-    margin: 10px;
-    cursor: default;
+  hr{
+      padding: 0;
+      border: none;
+      border-top: 5px solid #fff;
+      color: #fff;
+      text-align: center;
+      margin: 0px auto;
+      width: 420px;
+      height:10px;
+      z-index: -10;
+  }
 
-    .fade-in {
-    animation: fadeIn 2s ease infinite;
-    }
-}
+  hr:after {
+      content: "\2022";
+      display: inline-block;
+      position: relative;
+      top: -0.75em;
+      font-size: 2em;
+      padding: 0 0.2em;
+      background: #397be6;
+  }
 
-h1 {
-    font-size: 8em;
-    transition: font-size 200ms ease-in-out;
-    border-bottom: 1px dashed white;
+  .cloud {
+      width: 350px; height: 120px;
 
-    span#digit1 { animation-delay: 200ms; }
-    span#digit2 { animation-delay: 300ms; }
-    span#digit3 { animation-delay: 400ms; }
-}
+      background: #FFF;
+      background: linear-gradient(top, #FFF 100%);
+      background: -webkit-linear-gradient(top, #FFF 100%);
+      background: -moz-linear-gradient(top, #FFF 100%);
+      background: -ms-linear-gradient(top, #FFF 100%);
+      background: -o-linear-gradient(top, #FFF 100%);
 
-button {
-    border: 1px solid white;
-    background: transparent;
-    outline: none;
-    padding: 10px 20px;
-    font-size: 1.1rem;
-    font-weight: bold;
-    color: white;
-    text-transform: uppercase;
-    transition: background-color 200ms ease-in;
-    margin: 20px 0;
+      border-radius: 100px;
+      -webkit-border-radius: 100px;
+      -moz-border-radius: 100px;
 
-    &:hover {
-    background-color: white;
-    color: #555;
-    cursor: pointer;
-    }
-}
-}
-}
+      position: absolute;
+      margin: 120px auto 20px;
+      z-index:-1;
+      transition: ease 1s;
+  }
 
-@keyframes colorSlide {
-0% { background-color: #152a68; }
-25% { background-color: royalblue; }
-50% { background-color: seagreen; }
-75% { background-color: tomato; }
-100% { background-color: #152a68; }
-}
+  .cloud:after, .cloud:before {
+      content: '';
+      position: absolute;
+      background: #FFF;
+      z-index: -1
+  }
 
-@keyframes fadeIn {
-from {
-opacity: 0;
-};
-100% {
-opacity: 1;
-}
-}
+  .cloud:after {
+      width: 100px; height: 100px;
+      top: -50px; left: 50px;
+
+      border-radius: 100px;
+      -webkit-border-radius: 100px;
+      -moz-border-radius: 100px;
+  }
+
+  .cloud:before {
+      width: 180px; height: 180px;
+      top: -90px; right: 50px;
+
+      border-radius: 200px;
+      -webkit-border-radius: 200px;
+      -moz-border-radius: 200px;
+  }
+
+  .x1 {
+      top:-50px;
+      left:100px;
+      -webkit-transform: scale(0.3);
+      -moz-transform: scale(0.3);
+      transform: scale(0.3);
+      opacity: 0.9;
+      -webkit-animation: moveclouds 15s linear infinite;
+      -moz-animation: moveclouds 15s linear infinite;
+      -o-animation: moveclouds 15s linear infinite;
+  }
+
+  .x1_5{
+      top:-80px;
+      left:250px;
+      -webkit-transform: scale(0.3);
+      -moz-transform: scale(0.3);
+      transform: scale(0.3);
+      -webkit-animation: moveclouds 17s linear infinite;
+      -moz-animation: moveclouds 17s linear infinite;
+      -o-animation: moveclouds 17s linear infinite;
+  }
+
+  .x2 {
+      left: 250px;
+      top:30px;
+      -webkit-transform: scale(0.6);
+      -moz-transform: scale(0.6);
+      transform: scale(0.6);
+      opacity: 0.6;
+      -webkit-animation: moveclouds 25s linear infinite;
+      -moz-animation: moveclouds 25s linear infinite;
+      -o-animation: moveclouds 25s linear infinite;
+  }
+
+  .x3 {
+      left: 250px; bottom: -70px;
+
+      -webkit-transform: scale(0.6);
+      -moz-transform: scale(0.6);
+      transform: scale(0.6);
+      opacity: 0.8;
+
+      -webkit-animation: moveclouds 25s linear infinite;
+      -moz-animation: moveclouds 25s linear infinite;
+      -o-animation: moveclouds 25s linear infinite;
+  }
+
+  .x4 {
+      left: 470px; botttom: 20px;
+
+      -webkit-transform: scale(0.75);
+      -moz-transform: scale(0.75);
+      transform: scale(0.75);
+      opacity: 0.75;
+
+      -webkit-animation: moveclouds 18s linear infinite;
+      -moz-animation: moveclouds 18s linear infinite;
+      -o-animation: moveclouds 18s linear infinite;
+  }
+
+  .x5 {
+      left: 200px; top: 300px;
+
+      -webkit-transform: scale(0.5);
+      -moz-transform: scale(0.5);
+      transform: scale(0.5);
+      opacity: 0.8;
+
+      -webkit-animation: moveclouds 20s linear infinite;
+      -moz-animation: moveclouds 20s linear infinite;
+      -o-animation: moveclouds 20s linear infinite;
+  }
+
+  @-webkit-keyframes moveclouds {
+      0% {margin-left: 1000px;}
+      100% {margin-left: -1000px;}
+  }
+  @-moz-keyframes moveclouds {
+      0% {margin-left: 1000px;}
+      100% {margin-left: -1000px;}
+  }
+  @-o-keyframes moveclouds {
+      0% {margin-left: 1000px;}
+      100% {margin-left: -1000px;}
+  }
 </style>
 
-
-
-<div class="flex-container">
-    <div class="text-center">
-      <h1>
-        <span class="fade-in" id="digit1">4</span>
-        <span class="fade-in" id="digit2">0</span>
-        <span class="fade-in" id="digit3">4</span>
-      </h1>
-      <h3 class="fadeIn">PAGE NOT FOUND</h3>
-      <button type="button" name="button">Return To Home</button>
-    </div>
-  </div>
+<div id="clouds">
+    <div class="cloud x1"></div>
+    <div class="cloud x1_5"></div>
+    <div class="cloud x2"></div>
+    <div class="cloud x3"></div>
+    <div class="cloud x4"></div>
+    <div class="cloud x5"></div>
+</div>
+<div class='c'>
+    <div class='_404'>404</div>
+    <hr>
+    <div class='_1'>THE PAGE</div><br>
+    <div class='_2'>WAS NOT FOUND</div><br>
+    <a class='btn' href='{{ route('dashboard') }}'>BACK TO MARS</a>
+</div>
